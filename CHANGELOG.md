87d7078664f934a808a4387c8b125a1a43050273
Ryan Atkinson <mail@ryanatkn.com>
2023-10-30 22:24:43 -0600

    publish v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# @feltjs/felt_habitat
+
+## 0.1.0
+
+### Minor Changes
+
+- 9d22431: init
--- src/lib/package.ts
+++ src/lib/package.ts
@@ -6,7 +6,7 @@ export const package_json = {
 	name: '@feltjs/felt_habitat',
 	description:
 		'a proof-of-concept Svelte component for collaborative simulated habitats using Felt',
-	version: '0.0.1',
+	version: '0.1.0',
 	public: true,
 	icon: '🌵',
 	license: 'Unlicense',
